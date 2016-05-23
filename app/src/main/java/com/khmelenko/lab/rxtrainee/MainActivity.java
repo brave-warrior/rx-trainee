e9274eaeb101bd97fe6a4187b7a3fffc6bdb3f24
Dmytro Khmelenko <dmitriy-broker@rambler.ru>
2016-05-23 23:34:47 +0200

    Initial commit

---
+++ app/src/main/java/com/khmelenko/lab/rxtrainee/MainActivity.java
@@ -0,0 +1,13 @@
+package com.khmelenko.lab.rxtrainee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
